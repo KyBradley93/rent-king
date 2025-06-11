f4b01911397acf4f32aace1aeedfb892e9462317
Kyle Bradley <kylebradley@Mac.lan>
2025-06-11 15:50:53 -0400

    connected database to app

--- app.js
+++ app.js
@@ -1,9 +1,21 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const pool = require('./db');
 
-app.get('/', (req, res) => {
-  res.send('Hello Fucking World!')
+app.get('/', async (req, res) => {
+  try {
+    const results = await pool.query('SELECT * FROM tvs');
+    const tvs = results.rows;
+    res.send(`
+    <h1>Hello Fucking World!</h1>
+    <h2>Test Data from "tvs"</h2>
+    <pre>${JSON.stringify(tvs, null, 2)}</pre>
+  `);
+  } catch (err) {
+    console.error('error querying tvs', err);
+    res.status(500).send('Error: Could not query tvs table')
+  }
 })
 
 app.listen(port, () => {
